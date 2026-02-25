a46c11c2091ec48200cc5ec700c871bc25105f9a
trieucau <trieupulica77@gmail.com>
2026-02-25 21:36:48 +0700

    hoan thien chuc nang login lien ket firebase

--- eCommerce_Reactjs/src/utils/firebase.js
+++ eCommerce_Reactjs/src/utils/firebase.js
@@ -1,24 +1,16 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import { getAuth } from "firebase/auth";
+//modular v9
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  // apiKey: "AIzaSyC6BDR8vZuUHiqt7VQkhLJ3pxYroNNjntA",
-  // authDomain: "ecom-chat-1d35c.firebaseapp.com",
-  // projectId: "ecom-chat-1d35c",
-  // storageBucket: "ecom-chat-1d35c.appspot.com",
-  // messagingSenderId: "517909678281",
-  // appId: "1:517909678281:web:ee31dcdc6180e6a3cee518",
-  // measurementId: "G-3X0P8VH3KR"
-  apiKey: "AIzaSyD8jKMrbC-dKokIPnih4yrW1VK5dk3ETsg",
-  authDomain: "ecom-login-a89c0.firebaseapp.com",
-  projectId: "ecom-login-a89c0",
-  storageBucket: "ecom-login-a89c0.firebasestorage.app",
-  messagingSenderId: "296065512693",
-  appId: "1:296065512693:web:cb502131c5a7b1567bc1bc",
+  apiKey: "AIzaSyCdtfOTuT5t5_fv6VroGLE6-AFd_EuHElc",
+  authDomain: "khoaluan-login.firebaseapp.com",
+  projectId: "khoaluan-login",
+  storageBucket: "khoaluan-login.firebasestorage.app",
+  messagingSenderId: "550151094735",
+  appId: "1:550151094735:web:f3ce209f20109c805812ba",
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-export default firebase;
-export const authentication = getAuth(initializeApp(firebaseConfig));
+const app = initializeApp(firebaseConfig);
+
+export const authentication = getAuth(app);
--- ecomAPI/hashPassword.js
+++ ecomAPI/hashPassword.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcryptjs");
 
 const salt = bcrypt.genSaltSync(10);
 
-const password = "shipper123"; // đổi mật khẩu bạn muốn ở đây
+const password = "admin123"; // đổi mật khẩu bạn muốn ở đây
 
 const hash = bcrypt.hashSync(password, salt);
 
