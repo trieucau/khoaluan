8b545b881dca4c452780b5bd3fc5606a8da2157f
trieucau <trieupulica77@gmail.com>
2026-02-26 12:46:39 +0700

    cập nhật biến môi trường cho APP FIREBASE

--- eCommerce_Reactjs/src/utils/firebase.js
+++ eCommerce_Reactjs/src/utils/firebase.js
@@ -1,15 +1,14 @@
 //modular v9
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-require("dotenv").config();
 
 const firebaseConfig = {
-  apiKey: process.env.APP_FIREBASE_APIKEY,
-  authDomain: process.env.APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.APP_FIREBASE_APPID,
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
