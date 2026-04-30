372a8ed6e76912656aefa9068e1ffcbcad5c74ad
trieucau <trieupulica77@gmail.com>
2026-04-30 15:28:54 +0700

    fix(top-menu): thay đổi nội dụng hiển thị tăng trải nghiệm

--- eCommerce_Reactjs/src/container/Header/TopMenu.js
+++ eCommerce_Reactjs/src/container/Header/TopMenu.js
@@ -1,62 +1,32 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useRef } from 'react';
 import './Header.scss';
 
+const BRANDS = [
+  'GUCCI', 'CHANEL', 'LOUIS VUITTON', 'PRADA', 'DIOR',
+  'HERMÈS', 'BURBERRY', 'ARMANI', 'BALENCIAGA', 'GIVENCHY',
+  'YVES SAINT LAURENT', 'VERSACE', 'VALENTINO', 'FENDI', 'BOTTEGA VENETA',
+  'TOM FORD', 'RALPH LAUREN', 'CALVIN KLEIN', 'TOMMY HILFIGER', 'HUGO BOSS'
+];
+
 const TopMenu = (props) => {
-  const handleLogout = () => {
-    localStorage.removeItem('userData');
-    localStorage.removeItem('token');
-    window.location.href = '/login';
-  };
-
-  const name =
-    props.user && props.user.id
-      ? `${props.user.firstName || ''} ${props.user.lastName || ''}`.trim()
-      : '';
-
   return (
     <div className="top_menu">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-7">
-            <div className="float-left">
-              <p>
-                <i className="fa-solid fa-phone" style={{ fontSize: '11px' }} />
-                19006868
-              </p>
-              <p>
-                <i className="fa-solid fa-envelope" style={{ fontSize: '11px' }} />
-                solanashop77@gmail.com
-              </p>
+      <div className="top-marquee-container">
+        <div className="top-marquee-track">
+          {/* Mảng 1 */}
+          {BRANDS.map((brand, index) => (
+            <div key={`brand1-${index}`} className="brand-item">
+              <i className="fa-solid fa-star" style={{ fontSize: '10px', marginRight: '10px', color: '#F8B195' }}></i>
+              {brand}
             </div>
-          </div>
-          <div className="col-lg-5">
-            <div className="float-right">
-              <ul className="right_side">
-                <li>
-                  {props.user && props.user.id ? (
-                    <NavLink to={`/user/detail/${props.user.id}`}>
-                      {name}
-                    </NavLink>
-                  ) : (
-                    <NavLink to="/login">Đăng nhập</NavLink>
-                  )}
-                </li>
-                <li>
-                  {props.user && props.user.id ? (
-                    <a onClick={handleLogout} style={{ cursor: 'pointer' }}>
-                      Đăng xuất
-                    </a>
-                  ) : (
-                    <NavLink to="/register">Đăng ký</NavLink>
-                  )}
-                </li>
-                <li>
-                  <a>VI</a>
-                </li>
-              </ul>
+          ))}
+          {/* Mảng 2 (Bản sao để lặp vô tận) */}
+          {BRANDS.map((brand, index) => (
+            <div key={`brand2-${index}`} className="brand-item">
+              <i className="fa-solid fa-star" style={{ fontSize: '10px', marginRight: '10px', color: '#F8B195' }}></i>
+              {brand}
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
